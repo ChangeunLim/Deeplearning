86a1362bcb0b009b265a19a79a0a4460e3af9af6
libertar <cksrms1030@naver.com>
2024-11-26 17:47:36 +0900

    update

--- untitled2.py
+++ untitled2.py
@@ -4,7 +4,7 @@ import numpy as np
 
 model = load_model("cow_marbling_model.h5")
 
-img_path = "C:/Users/kimyoungjin/Desktop/QC_cow_segmentation_3_069452_crop.jpg"  # 테스트할 이미지 경로
+img_path = "C:\\Users\\cksrm\\Deeplunning\\cow_datasets\\cow_11\\QC_cow_segmentation_1++_000025_crop.jpg"  # 테스트할 이미지 경로
 img = image.load_img(img_path, target_size=(128, 128))
 img_array = np.expand_dims(image.img_to_array(img) / 255.0, axis=0)
 
